8686400f168b5b6d5e8dc7affbefd107aeef0f7f
MOHAMMED-ANEES-M <mhdaneesm3@gmail.com>
2024-03-21 22:07:26 +0530

    multer updated

--- middleware/multer.js
+++ middleware/multer.js
@@ -3,13 +3,18 @@ const path = require('path');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-      cb(null, './uploads')
-    },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now()
-      cb(null, uniqueSuffix+file.originalname)
+    const uploadDir = './uploads';
+    // Check if the uploads directory exists, if not, create it
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir);
     }
-  })
+    cb(null, uploadDir);
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now();
+    cb(null, uniqueSuffix + file.originalname);
+  }
+});
   
   const upload = multer({ storage: storage })
 
